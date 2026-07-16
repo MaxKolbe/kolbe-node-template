7a833c33c02ffbdbe7ce1dbde6c9b62c4689d31e
Maxmillian <maximilianogbuabor@gmail.com>
2026-07-16 23:56:47 +0100

    fix

--- src/configs/vitest.setup.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// vitest.setup.ts
-var node_path_1 = require("node:path");
-process.loadEnvFile(node_path_1.default.resolve(import.meta.dirname, "../../.env"));
--- src/lib/events.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appEvents = void 0;
-var events_1 = require("events");
-exports.appEvents = new events_1.EventEmitter();
-exports.appEvents.setMaxListeners(20);
