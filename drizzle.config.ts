36047eae95f9337c31b7f407324342d6e82fb045
Maxmillian <maximilianogbuabor@gmail.com>
2026-07-16 23:17:21 +0100

    feat: add

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "drizzle-kit";
+
+const dbMap = new Map([
+  ["development", process.env.PG_DATABASE_DEV_URL!.toString()],
+  ["test", process.env.PG_DATABASE_TEST_URL!.toString()],
+  ["production", process.env.PG_DATABASE_PROD_URL!.toString()]
+])
+const dburl = dbMap.get(process.env.NODE_ENV!)
+
+export default defineConfig({   
+  out: "./drizzle",
+  dialect: "postgresql",
+  schema: "./src/db/models",  
+  dbCredentials: {
+    url: `${dburl}`
+    // used ?sslmode=verify-full to avoid adding the ssl property
+  },
+});
